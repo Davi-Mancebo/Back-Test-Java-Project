930f91fcc1d4945f12329d1ecdc7baeaa0eb5f97
unknown <davimancebo@gmail.com>
2023-10-24 00:07:51 -0300

    end of challenge 4, configuration of project

---
+++ back-end-test-java/src/main/java/br/com/test/backendtestjava/BackEndTestJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.test.backendtestjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndTestJavaApplication.class, args);
+	}
+
+}
---
+++ back-end-test-java/src/test/java/br/com/test/backendtestjava/BackEndTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.test.backendtestjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
